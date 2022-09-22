3107a66c00f14fbc5349abafe89e663f62b76e86
Egor <khromov.developer@mail.ru>
2022-09-23 00:36:53 +0300

    added popup

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -1,6 +1,8 @@
 <template>
   <div @click="hidePopup" class="flex absolute top-0 left-0 right-0 bottom-0 bg-gray-500 opacity-75">
-    <div @click.stop class="m-auto bg-white rounded-md shadow-md w-52 h-32"></div>
+    <div @click.stop class="m-auto bg-white rounded-md shadow-md">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
