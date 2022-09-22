9e6bf94ae11f9fec4a0caa14749c8fa509257ba0
Egor <khromov.developer@mail.ru>
2022-09-22 23:41:12 +0300

    added tailwind

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,13 +1,11 @@
 <template>
-  <MainPage></MainPage>
+  <div></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import MainPage from "@/pages/MainPage.vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {MainPage},
+  name: 'MainPage',
 });
 </script>
