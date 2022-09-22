75db62a011cbaf510bf9ea6789f23e52c56c9e8d
Egor <khromov.developer@mail.ru>
2022-09-23 00:35:26 +0300

    added popup

---
+++ src/components/PopUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="hidePopup" class="flex absolute top-0 left-0 right-0 bottom-0 bg-gray-500 opacity-75">
+    <div @click.stop class="m-auto bg-white rounded-md shadow-md w-52 h-32"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PopUp",
+  emits: ['hide-popup'],
+  methods: {
+    hidePopup() {
+      this.$emit('hide-popup');
+    }
+  }
+})
+</script>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="flex flex-col sm:flex-row mt-5 mx-5 sm:mx-10 lg:mx-20">
-    <MyButton class="bg-blue-500 mb-3 sm:mb-0 sm:mr-5">Заказать в Москву</MyButton>
-    <MyButton class="bg-teal-600">Заказать в Санкт-Петербург</MyButton>
+    <MyButton @click="setPopup(true)" class="bg-blue-500 mb-3 sm:mb-0 sm:mr-5">Заказать в Москву</MyButton>
+    <MyButton @click="setPopup(true)" class="bg-teal-600">Заказать в Санкт-Петербург</MyButton>
   </div>
+  <PopUp @hide-popup="setPopup(false)" v-show="popupVisible"></PopUp>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import MyButton from "@/components/MyButton.vue";
+import PopUp from "@/components/PopUp.vue";
 
 export default defineComponent({
   name: 'MainPage',
-  components: {MyButton},
+  components: {PopUp, MyButton},
+  data() {
+    return {
+      popupVisible: false,
+    }
+  },
+  methods: {
+    setPopup(value: boolean) {
+      this.popupVisible = value;
+    }
+  }
 });
 </script>
